fd46f5695ae20b16a9d32ae067cc82fa77a74421
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-10-07 22:14:29 -0700

    Better config

--- lib/alexandria.rb
+++ lib/alexandria.rb
@@ -8,16 +8,18 @@ require "erb"
 module Alexandria
   def self.ssh_client(client_info)
     config = Alexandria::Config.new
+
     user = Alexandria::User.new(ARGV[0])
     command = Alexandria::Command.new(ENV['SSH_ORIGINAL_COMMAND'])
     command.run(config, user)
   end
 
   def self.client
+    config = Alexandria::Config.new
+
     if ARGV[0] == "write_keys"
       puts "Generating authorized keys..."
       key_data = User.generate_keys
-      config = Alexandria::Config.new
       File.open(config.key_file, "w") {|f| f.write(key_data) }
       puts "Authorized keys written to: #{config.key_file}"
     elsif ARGV[0] == "list_users"
@@ -26,6 +28,8 @@ module Alexandria
       users.each do |user|
         puts " * #{user.name}"
       end
+    elsif ARGV[0] == "gen_config"
+      puts config.to_yaml
     else
       puts "Invalid command #{ARGV[0]}" if ARGV[0]
       puts "Usage:"
--- lib/alexandria/config.rb
+++ lib/alexandria/config.rb
@@ -1,13 +1,37 @@
+require 'yaml'
+
 module Alexandria
   class Config
     attr_accessor :key_file, :git_user, :repo_dir, :log_file
 
     def initialize
-      @git_user = "git"
-      @repo_dir = "/home/#{@git_user}/repos"
-      @key_file = "/home/#{@git_user}/.ssh/authorized_keys"
-      @log_file = "/var/log/alexandria.log"
-      @data = Alexandria::Data
+      if File.exists? "/etc/alexandria.conf"
+        data = File.open("/etc/alexandria.conf").read
+        raw = YAML.load(data)
+        if raw
+          raw.each_pair do |k,v|
+            send("#{k}=", v)
+          end
+        end
+      end
+
+      @git_user ||= "git"
+      @repo_dir ||= "/home/#{@git_user}/repos"
+      @key_file ||= "/home/#{@git_user}/.ssh/authorized_keys"
+      @log_file ||= "/var/log/alexandria.log"
+      @data ||= Alexandria::Data
+    end
+
+    def attributes
+      [:key_file, :git_user, :repo_dir, :log_file]
+    end
+
+    def to_yaml
+      raw = {}
+      attributes.each do |attr|
+        raw[attr] = send(attr)
+      end
+      YAML.dump(raw).to_s
     end
   end
 end
