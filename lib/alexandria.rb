ea04c970510e25381f9a44c1d995342686ffe07a
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-10-07 23:20:58 -0700

    Clean commands

--- lib/alexandria.rb
+++ lib/alexandria.rb
@@ -14,29 +14,30 @@ module Alexandria
       user = Alexandria::User.new(ARGV[1])
       command = Alexandria::Command.new(ENV['SSH_ORIGINAL_COMMAND'])
       command.run(config, user)
-    elsif ARGV[0] == "write_keys"
-      puts "Generating authorized keys..."
-      key_data = User.generate_keys
-      FileUtils.mkdir_p File.dirname(config.key_file)
-      File.open(config.key_file, "w") {|f| f.write(key_data) }
-      puts "Authorized keys written to: #{config.key_file}"
-    elsif ARGV[0] == "list_users"
-      puts "Getting user list..."
-      users = User.all
-      users.each do |user|
-        puts " * #{user.name}"
+    elsif ARGV[0] == "setup"
+      if ENV['USER'] == config.git_user
+        puts "Generating authorized keys..."
+        key_data = User.generate_keys
+        FileUtils.mkdir_p File.dirname(config.key_file)
+        FileUtils.mkdir_p File.join(config.repo_dir)
+        File.open(config.key_file, "w") {|f| f.write(key_data) }
+        puts "Authorized keys written to: #{config.key_file}"
+      else
+        puts "Please run setup as #{config.git_user}"
       end
-    elsif ARGV[0] == "gen_config"
-      puts config.to_yaml
-    elsif ARGV[0] == "create_repo"
-      FileUtils.mkdir_p File.join(config.repo_dir, ARGV[1])
-      FileUtils.chdir File.join(config.repo_dir, ARGV[1])
-      Kernel.exec 'git', 'init', '--bare'
+    elsif ARGV[0] == "create"
+      if ENV['USER'] == config.git_user
+        FileUtils.mkdir_p File.join(config.repo_dir, ARGV[1])
+        FileUtils.chdir File.join(config.repo_dir, ARGV[1])
+        Kernel.exec 'git', 'init', '--bare'
+      else
+        puts "Please run create as #{config.git_user}"
+      end
     else
       puts "Invalid command #{ARGV[0]}" if ARGV[0]
       puts "Usage:"
       puts
-      puts "  commands: list_users, write_keys"
+      puts "  commands: setup, create"
       puts
     end
   end
